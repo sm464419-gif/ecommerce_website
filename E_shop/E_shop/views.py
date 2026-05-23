daf7aa873533c45764f51b7255d6c82e5973fff5
sm464419-gif <sm464419@gmail.com>
2026-05-23 21:15:18 +0600

    add

---
+++ E_shop/E_shop/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render,redirect
+from app.models import Category,Product
+from django.contrib.auth import authenticate,login
+from app.models import UserCreateForm
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
+
+
+# Create your views here.
+def Base(request):
+    return render(request,'base.html')
+
+
+def Index(request):
+    category= Category.objects.all()
+    product= Product.objects.all()
+    context={
+        'category' : category,
+        'product': product,
+    }
+    return render(request, 'index.html',context)
+
+def signup(request):
+    if request.method=='POST':
+        form= UserCreateForm(request.POST)
+        if form.is_valid():
+            new_user= form.save()
+            new_user= authenticate(
+                username = form.cleaned_data['username'],
+                password= form.cleaned_data['password1']
+            )
+            login(request,new_user)
+            return redirect('index')
+
+    else:
+        form = UserCreateForm()
+
+    context={
+        'form': form,
+    }
+
+    return render(request,'registration/signup.html',context)
+
+
+@login_required(login_url="/accounts/login/")
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("index")
+
+
+@login_required(login_url="accounts/login/")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="accounts/login/")
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="accounts/login/")
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="accounts/login/")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required(login_url="accounts/login/")
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
+
+def Contact_Page(request):
+    if request.method =="POST":
+        contact = Contact_us(
+            name= request.POST.get('name'),
+            email=request.POST.get('email'),
+            subject=request.POST.get('subject'),
+            message=request.POST.get('message'),
+        )
+        contact.save()
+
+    return render(request, 'contact.html')
---
+++ E_shop/app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from.models import Category,Subcategory,Product,Contact_us,Order,Brand
+
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Product)
+admin.site.register(Contact_us)
+admin.site.register(Order)
+admin.site.register(Brand)
+
+
+# Register your models here.
